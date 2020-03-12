86ee754f92d7caf2492fd60ff80daf3f90336e5b
David Seka <davidseka2000@gmail.com>
2020-03-12 21:07:15 +0100

    Changed history to Stack and added a scanner.

--- NewGame_Project/src/com/company/Main.java
+++ NewGame_Project/src/com/company/Main.java
@@ -7,15 +7,19 @@ public class Main {
     public static void main(String[] args) {
         DynamicConsole myConsole = new DynamicConsole();
         myConsole.println("Hello Word");
-        myConsole.println("a\na\na\na\na\na\na");
-        myConsole.println("b\nb\nb\nb\nb\nb\nb");
-        myConsole.println("c\nc\nc\nc\nc\nc\nc");
+        myConsole.println("a\naa\naaa");
+        myConsole.println("b\nbb\nbbb");
+        myConsole.println("c\ncc\nccc");
 
         try {
             Thread.sleep(2_000);
         } catch(Exception e){}
 
-        myConsole.replace(1, "d\nd\nd\nd\nd\nd\nd");
+        myConsole.replace(1, "d\ndd\nddd");
+
+        myConsole.nextDouble();
+
+        myConsole.revert(3);
 
         try {
             Thread.sleep(2_000);
--- NewGame_Project/src/com/company/Output/DynamicConsole.java
+++ NewGame_Project/src/com/company/Output/DynamicConsole.java
@@ -1,34 +1,30 @@
 package com.company.Output;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
 
+/**
+ * This class allows to write on the console and gives the user the ability to revert the changes.
+ * Also, the user should not use System.out.print(ln)() outside of this class.
+ * It's best to just create one instance of this console (Maybe the methods should be static)
+ */
 public class DynamicConsole {
 
     /**
      * History saves everything that has been written on the console
      */
-    private ArrayList<String> history;
+    private Stack<String> history;
 
     /**
-     * indicates, how many lines have been deleted
+     * Scanner to handle input
      */
-    private int historyPointer;
+    private Scanner cin;
+
 
     public DynamicConsole(){
-        history = new ArrayList<>();
-        historyPointer = 0;
-    }
-
-    /**
-     * Equivalent of System.out.print();
-     * If you are using the dynamic console class, make sure to replace all System.out.print(); with this method.
-     * @param output String to print
-     */
-    public void print(String output){
-        assert (historyPointer == history.size());
-        history.add(output);
-        historyPointer++;
-        System.out.print(output);
+        history = new Stack<>();
+        cin = new Scanner(System.in);
     }
 
     /**
@@ -37,9 +33,9 @@ public class DynamicConsole {
      * @param output String to print
      */
     public void println(String output){
-        assert (historyPointer == history.size());
         output += "\n";
-        print(output);
+        history.push(output);
+        System.out.print(output);
     }
 
     /**
@@ -47,41 +43,115 @@ public class DynamicConsole {
      * @param printsBefore
      */
     public void replace(int printsBefore, String replacingString){
+        Stack<String> inverseStack = new Stack();
         int linesToClear = 0;
 
-        for(int i = printsBefore; i >= 0; i--){
-            linesToClear += lines(getHistoryFromLast(i));
+        for(int i = 0; i <= printsBefore; i++){
+            String stringToPop = history.pop();
+            linesToClear += lines(stringToPop);
+            inverseStack.push(stringToPop);
         }
 
         clearLines(linesToClear);
 
         StringBuilder sb = new StringBuilder(replacingString + "\n");
 
-        for(int i = printsBefore - 1; i >= 0; i--){
-            sb.append(getHistoryFromLast(i));
+        inverseStack.pop();         //pops the vale to delete from the inverse stack
+        history.push(replacingString + "\n");
+
+        for(int i = 0; i < printsBefore; i++){
+            sb.append(inverseStack.peek());
+            history.push(inverseStack.pop());
         }
 
         System.out.print(sb);
+    }
 
+    /**
+         * Equivalent of scanner.nextInt(); Saves the int in history, however
+         * @return
+         */
+        public int nextInt(){
+            int i = 0;
+            boolean rep = true;
+
+            while (rep) {
+                try {
+                    String in = cin.nextLine();
+                    i = Integer.parseInt(in);
+                    rep = false;
+                } catch(NumberFormatException e){
+                    System.out.print("Integer expected, try again after this message disappears!\n");
+
+                    try {
+                        Thread.sleep(1_000);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+
+                    clearLines(2);
+                }
+            }
+
+        history.push(i + "\n");
+
+        return i;
+    }
+
+    /**
+     * Equivalent of scanner.nextDouble(); Saves the int in history, however
+     * @return
+     */
+    public double nextDouble(){
+        double d = 0;
+        boolean rep = true;
+
+        while (rep) {
+            try {
+                String in = cin.nextLine();
+                d = Double.parseDouble(in);
+                rep = false;
+            } catch(NumberFormatException e){
+                System.out.print("Double expected, try again after this message disappears!\n");
+
+                try {
+                    Thread.sleep(1_000);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+
+                clearLines(2);
+            }
+        }
+
+        history.push(d + "\n");
+
+        return d;
+    }
+
+    /**
+     * Equivalent of scanner.nextLine(); saves the line in history, however
+     */
+    public String nextLine(){
+        String in = cin.nextLine();
+        history.push(in + "\n");
+        return in;
     }
 
     /**
      * Deletes the last print from the history from the console
      */
     public void revert(){
-        String stringToClear = history.get(history.size() - 1);
-        int linesToClear = lines(stringToClear);
-        clearLines(linesToClear);
-        historyPointer--;
-        dump();
+        clearLines(lines(history.pop()));
     }
 
     /**
-     * Deletes all Strings after historyPointer form history
+     * Deletes the last prints from the history from the console
+     * @param printsToRevert The number of prints that should be reverted
      */
-    public void dump(){
-        for(int i = history.size() - 1; i > historyPointer; i--){
-            history.remove(i);
+    public void revert(int printsToRevert){
+        for(int i = 0; i < printsToRevert; i++){
+            revert();
         }
     }
 
@@ -92,7 +162,7 @@ public class DynamicConsole {
      */
     private int lines(String str){
         char newLine = (char) 10;
-        int lines = 1;                   //if a stirng has no \n, there is still a line
+        int lines = 0;                   //if a stirng has no \n, there is still a line
 
         for(char c : str.toCharArray()){
             if(c == newLine){
@@ -110,7 +180,18 @@ public class DynamicConsole {
      */
     void clearLines(int lines){
         for(int i = 0; i< lines; i++) {
+            System.out.print("\033[1A");
             clearLine();
+        }
+    }
+
+    /**
+     * Clears multiple above the cursor.
+     * "\033[1A" jumps one line up
+     * @param lines lines to jump
+     */
+    void jumpLines(int lines){
+        for(int i = 0; i< lines; i++) {
             System.out.print("\033[1A");
         }
     }
@@ -122,22 +203,5 @@ public class DynamicConsole {
      */
     public void clearLine(){
         System.out.print("\r\033[K");
-    }
-
-    /**
-     * This calls history.get(history.size() - 1 - before);
-     * @param before
-     * @return
-     */
-    private String getHistoryFromLast(int before){
-        return history.get(history.size() - 1 - before);
-    }
-
-    /**
-     * This calls history.get(history.size() - 1 - before);
-     * @return
-     */
-    private String getLastHistory(){
-        return history.get(history.size() - 1);
     }
 }
